26a32448b183dd0d4e74d48f7c5cf1a5acae7583
cuu-vnpt <kunnobita97@gmail.com>
2023-12-19 23:31:02 +0700

    new file

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿class Program 
+{
+    public static void Main(string[] args)
+    {
+        int Choise = -1; 
+        while (Choise!=0)
+        {
+            Console.WriteLine("Nhap mot ky tu tu ban phim: "); 
+            Console.WriteLine("1. Draw the triangle: ");
+            Console.WriteLine("2. Draw the square: ");
+            Console.WriteLine("3. Draw the rectangle: ");
+            Console.WriteLine("0. Exit ");
+            Console.WriteLine("Enter your choice: ");
+            Choise = Convert.ToInt32(Console.ReadLine());
+            switch (Choise)
+            {
+                case 1: 
+                {
+                    Console.WriteLine("************");
+                    Console.WriteLine("**********");
+                    Console.WriteLine("********");
+                    Console.WriteLine("*******");
+                    Console.WriteLine("******");
+                    Console.WriteLine("*****");
+                    Console.WriteLine("****");
+                    Console.WriteLine("***");
+                    Console.WriteLine("**");
+                    Console.WriteLine("*");
+                }
+                break;
+                case 2:
+                {
+                    Console.WriteLine("************");
+                    Console.WriteLine("************");
+                    Console.WriteLine("************");
+                    Console.WriteLine("************");
+                    Console.WriteLine("************");
+                }
+                break;
+                case 3:
+                {
+                    Console.WriteLine("************");
+                    Console.WriteLine("************");
+                    Console.WriteLine("************");
+                }
+                break;
+                case 4:
+                {
+                    Environment.Exit(0);
+                    
+                }
+                break;
+
+                default: Console.WriteLine("Nhap lai");
+                break;
+            }
+        }
+    }
+}
